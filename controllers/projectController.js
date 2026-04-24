341f69357184a9d81006cee866a92fd68aead7a0
naimursafi2 <naimursafi4@gmail.com>
2026-04-24 13:09:54 +0600

    membersadd

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,17 +1,18 @@
 const { generateSlug } = require("../helpers/utils");
+const authSchema = require("../models/authSchema");
 const projectSchema = require("../models/projectSchema");
 
 const createProject = async (req, res) => {
   const { title, description } = req.body;
   try {
     const slug = generateSlug(title);
-    const project = await projectSchema({
+    const project = new projectSchema({
       title,
       description,
       slug,
       author: req.user._id,
     });
-    project.save();
+    await project.save();
     res.status(200).send({ message: "project created successfully" });
   } catch (error) {
     console.log(error);
@@ -23,13 +24,15 @@ const projectList = async (req, res) => {
   try {
     const { search } = req.query;
 
-    const project = await projectSchema.find({
-      author: req.user._id,
-      title: {
-        $regex: search,
-        $options: "i",
-      },
-    }).populate("author","fullName avatar");
+    const project = await projectSchema
+      .find({
+        author: req.user._id,
+        title: {
+          $regex: search,
+          $options: "i",
+        },
+      })
+      .populate("author", "fullName avatar");
     if (!project) return res.status(400).send({ message: "Project not found" });
 
     res.status(200).send({ project });
@@ -37,4 +40,25 @@ const projectList = async (req, res) => {
     res.status(500).send({ message: "Internal Server Error!" });
   }
 };
-module.exports = { createProject, projectList };
+
+const addTeamMemberToProject = async (req, res) => {
+  const { email, projectId } = req.body;
+  try {
+    const existEmail = await authSchema.findOne({ email });
+    if (!existEmail)
+      return res.status(400).send({ message: "Email is not exist" });
+
+    const project = await projectSchema.findOneAndUpdate(
+      { _id: projectId },
+      { members: existEmail._id },
+      { new: true },
+    );
+    if (!project) return res.status(400).send({ message: "Invalid Request" });
+
+    console.log(existEmail);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "internal server Error" });
+  }
+};
+module.exports = { createProject, projectList, addTeamMemberToProject };
--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createProject, projectList } = require("../controllers/projectController");
+const { createProject, projectList, addTeamMemberToProject } = require("../controllers/projectController");
 const router = express.Router();
 
 router.post("/create",createProject)
 router.get("/list",projectList)
+router.post("/addmember",addTeamMemberToProject)
 
 module.exports = router
